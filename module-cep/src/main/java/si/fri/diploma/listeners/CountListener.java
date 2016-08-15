6c64fb937189c9d468b23c5a39b6b8ec41d26fd7
NaumGj <naum_gj@hotmail.com>
2016-08-15 13:05:10 +0200

    Latency and count working 2

---
+++ module-cep/src/main/java/si/fri/diploma/listeners/CountListener.java
@@ -0,0 +1,48 @@
+package si.fri.diploma.listeners;
+
+import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import com.espertech.esper.client.UpdateListener;
+import com.espertech.esper.client.EventBean;
+
+import si.fri.diploma.models.CountsObject;
+
+@ApplicationScoped
+public class CountListener implements UpdateListener {
+	
+	public static final Logger LOG = Logger.getLogger(CountListener.class.getName());
+	
+	private static final Integer LIST_SIZE = 30;
+	private static LinkedList<CountsObject> counts = new LinkedList<CountsObject>();
+	
+    public void update(EventBean[] newEvents, EventBean[] oldEvents) {
+        if (newEvents == null) {
+            return; // ignore old events for events leaving the window
+        }
+
+        // CHECK IF newEvents ARRAY ALWAYS HAS 1 ELEMENT
+        LOG.log(Level.INFO, "LENGTH: " + newEvents.length);
+        EventBean theEvent = newEvents[0];
+
+//        log.info("count=" + theEvent.get("cnt").toString());
+        if(theEvent != null) {
+        	Long cnt = (Long)theEvent.get("cnt");
+        	if (cnt != null) {
+	        	CountsObject count = new CountsObject();
+	        	count.setCounts(cnt);
+	        	counts.addLast(count);
+	        	while (counts.size() > LIST_SIZE) {
+	        		counts.removeFirst();
+	        	}
+        	}
+        }
+    }
+
+    public LinkedList<CountsObject> getCounts() {
+    	return counts;
+    }
+}
